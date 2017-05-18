42a9f48d93255bb7970cda3590a8ee49632f5174
Ben Newman <ben@benjamn.com>
2017-05-18 17:31:42 -0400

    Use module.export(getters, true) for const ExportNamedDeclaration.
    These changes let the runtime know that the values returned by the getter
    functions for
    
      export const a = 1, b = 2;
    
    will never change. Unfortunately, it's more difficult to detect that the
    same is true for
    
      const a = 1, b = 2;
      export { a, b };
    
    though that's an area for future improvement.
    
    Part of #134.

--- test/output/name/expected.js
+++ test/output/name/expected.js
@@ -1,4 +1,4 @@
-"use strict";module.export({id:()=>id,name:()=>name,foo:()=>foo});const path = require("path");
+"use strict";module.export({foo:()=>foo});module.export({id:()=>id,name:()=>name},true);const path = require("path");
 
 const id = module.id,
   name = path.basename(__filename);
