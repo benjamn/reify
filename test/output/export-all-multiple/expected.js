755059ce2cf8a231c25c28891ab6b2fd3cd36486
Ben Newman <ben@benjamn.com>
2018-01-08 20:07:47 -0500

    Hoist export...from declarations in order with imports.
    This fixes the failing test that I added in the previous commit.
    
    https://github.com/meteor/meteor/issues/9504

--- test/output/export-all-multiple/expected.js
+++ test/output/export-all-multiple/expected.js
@@ -1,1 +1,1 @@
-"use strict";module.export({Abc:()=>n});module.watch(require("./def"),{"*":module.makeNsSetter()},1);var n;module.watch(require("./abc"),{"*"(v){n=v}},0);
+"use strict";module.export({Abc:()=>n});var n;module.watch(require("./abc"),{"*"(v){n=v}},0);module.watch(require("./def"),{"*":module.makeNsSetter()},1);
