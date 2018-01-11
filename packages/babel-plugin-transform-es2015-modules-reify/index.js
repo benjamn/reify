46109280c250776cbd0b75b7a7c870e9b8708be0
Ben Newman <ben@benjamn.com>
2018-01-11 18:32:14 -0500

    Drastically simplify Babel plugin by calling cache.clearScope().
    Instead of attempting to update Scope objects after modifying the AST with
    require("reify/lib/compiler").transform, it turns out we can simply forget
    any Scopes that have been created previously!

--- packages/babel-plugin-transform-es2015-modules-reify/index.js
+++ packages/babel-plugin-transform-es2015-modules-reify/index.js
@@ -1,19 +1,23 @@
 var namespace;
 var types;
+var cache;
 
 function tryScoped() {
   namespace = require("@babel/types");
-  types = require("@babel/types");
+  types = types || require("@babel/types");
+  cache = cache || require("@babel/traverse").default.cache;
 }
 
 function tryScopedLib() {
   namespace = require("@babel/types/lib/validators");
-  types = require("@babel/types");
+  types = types || require("@babel/types");
+  cache = cache || require("@babel/traverse").default.cache;
 }
 
 function tryUnscoped() {
   namespace = require("babel-types/lib/validators");
-  types = require("babel-types");
+  types = types || require("babel-types");
+  cache = cache || require("babel-traverse").default.cache;
 }
 
 [tryScoped,
@@ -41,71 +45,9 @@ function tryUnscoped() {
 module.exports = function (context) {
   var compiler = require("reify/lib/compiler.js");
   var parse = require("reify/lib/parsers/babylon.js").parse;
-  var t = types || context.types;
-
-  function removeLiveBindingUpdateViolations(scope) {
-    Object.keys(scope.bindings).forEach(function (name) {
-      var b = scope.bindings[name];
-      // Ignore constant violations from inside module.import(id, {...})
-      // callback functions, since they are necessary for updating
-      // imported symbols to simulate live bindings. The beauty of this
-      // solution is that babel-plugin-check-es2015-constants will still
-      // forbid any other reassignments of imported symbols, which
-      // enforces the const-ness of the live-bound variables. The
-      // Array#{splice,forEach,push} idiom is similar to Array#filter,
-      // except it preserves the original array object.
-      b.constantViolations.splice(0).forEach(function (cv) {
-        if (false && ! isPartOfImportMethodCall(cv)) {
-          b.constantViolations.push(cv);
-        }
-      });
-    });
-  }
-
-  function isPartOfImportMethodCall(path) {
-    for (var path = path.scope.path;
-         path && ! t.isStatement(path.node);
-         path = path.parentPath) {
-      if (isImportCallExpression(path.node)) {
-        return true;
-      }
-    }
-  }
-
-  function isImportCallExpression(node) {
-    return t.isCallExpression(node) &&
-      t.isMemberExpression(node.callee) &&
-      ! node.callee.computed &&
-      t.isIdentifier(node.callee.property) &&
-      // Note that we don't check if node.callee.object is the `module`
-      // identifier, because we may want to remap that reference to a
-      // unique temporary variable, which would make it difficult to know
-      // what the variable name should be.
-      ["import", "importSync", "watch"].indexOf(
-        node.callee.property.name
-      ) >= 0
-  }
 
   return {
     visitor: {
-      Scope: {
-        enter: function (path) {
-          removeLiveBindingUpdateViolations(path.scope);
-        },
-
-        exit: function (path) {
-          var hasModuleBindings = Object.keys(
-            path.scope.bindings
-          ).some(function (name) {
-            return path.scope.bindings[name].kind === "module";
-          });
-
-          if (hasModuleBindings) {
-            removeLiveBindingUpdateViolations(path.scope);
-          }
-        }
-      },
-
       Program: function (path) {
         var transformOptions = {
           parse: parse
@@ -121,6 +63,10 @@ module.exports = function (context) {
           path.node,
           Object.assign(transformOptions, this.opts)
         );
+
+        // Invalidate all Scope objects, so that variable binding changes
+        // made by compiler.transform will be reflected accurately.
+        cache.clearScope();
       }
     }
   };
