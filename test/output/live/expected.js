159b2a5c2737ad2ed47d0f33e4792dbc545d1416
zodern <zodern@users.noreply.github.com>
2021-11-26 14:44:05 -0600

    Pass name of modified export to runSetters

--- test/output/live/expected.js
+++ test/output/live/expected.js
@@ -1,9 +1,9 @@
 "use strict";module.export({value:()=>value,reset:()=>reset,add:()=>add});var value = reset();
 
 function reset() {
-  return module.runSetters(value = 0);
+  return module.runSetters(value = 0,"value");
 }
 
 function add(x) {
-  module.runSetters(value += x);
+  module.runSetters(value += x,"value");
 };
